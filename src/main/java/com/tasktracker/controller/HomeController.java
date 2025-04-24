c19e0cf3b99ee1d01201a187ade745ebc349ca7f
DooMi- <hurmerintaeemeli@gmail.com>
2025-04-25 00:45:13 +0300

    quick update to the project.

---
+++ src/main/java/com/tasktracker/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tasktracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Task & Time Tracker API is running 🚀";
+    }
+}
